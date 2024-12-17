19bc875c6e30eb6f00ec630ffaf023f5fa4f42c6
梁润铎 <liangrunduo@liangrunduodeMacBook-Pro.local>
2024-12-17 18:54:09 +0800

    update

--- EPTools/plot.py
+++ EPTools/plot.py
@@ -14,13 +14,19 @@ def plot_gcn_data(file_dir='',output='standard_gcn.pdf'):
 
     Alldata = pd.read_csv(file_dir).sort_values(by='dt').reset_index(drop=True)
     types = np.unique(data['type'])
-    for ty in types:
+    
+    fig = plt.figure(figsize=(11,9),dpi=200)
+    gs = fig.add_gridspec(len(types), hspace=0,height_ratios=np.array([1.5,2,1]))
+    ax = gs.subplots(sharex=True)
+    
+    for i,ty in enumerate(types):
         data = Alldata[Alldata['type']==ty]
         t = data['dt']
         if ty == 'optical':
             y = data['mag']
             yerr = data['magerr']
             ylabel = 'mag'
+            ax[i].invert_yaxis()
         elif ty == 'x-ray' or ty == 'radio':
             y = data['flux']
             yerr = data['fluxerr']
@@ -30,6 +36,12 @@ def plot_gcn_data(file_dir='',output='standard_gcn.pdf'):
                 ylabel = r'$\mu Jy$'
         else:
             raise KeyError('Invalid observation type!')
+        
+        ax[i].errorbar(x=t,y=y,yerr=yerr)
+        ax[i].set_ylabel(ylabel)
+        if i == len(types)-1:
+            ax[i].set_xlabel('t(day)')
             
+    plt.savefig(output,dpi=300)
     
 
