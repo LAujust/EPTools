7a161ba520041ed29cf3b718ec919b71d971d3cc
梁润铎 <loyalaujust@outlook.com>
2025-02-20 22:46:01 +0800

    update

--- EPTools/gw_em.py
+++ EPTools/gw_em.py
@@ -1,7 +1,7 @@
 'GW-EM'
 from .utils import *
 sys.path.append('$HEADAS/lib/python')
-from xspec import *
+from xspec import * 
 'A module to transform synthetic model SED file to observed SED of X-ray detector'
 'With the help of PyXspec'
 
