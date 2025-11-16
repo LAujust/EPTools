dd1d682aef109ae49e5837a5a2f1b3e09c702736
梁润铎 <loyalaujust@outlook.com>
2025-11-16 23:29:46 +0800

    update cosmic_ray

--- EPTools/utils.py
+++ EPTools/utils.py
@@ -349,6 +349,67 @@ def Txx(times,rates,c=0.9):
     T5, T95 = times[i5], times[i95]
     return T95-T5
 
+
+def check_cosmic_ray(path:str,s_num:int,cts_thres:float=5):
+    
+    cat_hdu = fits.open(glob.glob(os.path.join(path,'**.cat')))
+    evt_hdu = fits.open(glob.glob(os.path.join(path,'**po_cl.evt')))
+    uf_hdu = fits.open(glob.glob(os.path.join(path,'**po_uf.evt')))
+
+    hdudata = cat_hdu[1].data
+    x0 = hdudata['x'][s_num-1]
+    y0 = hdudata['y'][s_num-1]
+
+    x0 = x0 * 16
+    y0 = y0 * 16
+    hdudata = evt_hdu[1].data
+    data_cl = np.transpose(np.vstack((hdudata['time'], hdudata['rawx'], hdudata['rawy'], hdudata['x'], hdudata['y'], hdudata['pi'])))
+    data_cl = pd.DataFrame(data_cl, columns=['time', 'rawx', 'rawy', 'x', 'y', 'pi'])
+
+    hdudata = uf_hdu[1].data
+    data_uf = np.transpose(np.vstack((hdudata['time'], hdudata['x'], hdudata['y'], hdudata['pi'], hdudata['cmosfram'])))
+    data_uf = pd.DataFrame(data_uf, columns=['time', 'x', 'y', 'pi', 'cmosfram'])
+
+    data = pd.merge(data_cl, data_uf, how='left', on=['time', 'x', 'y', 'pi'])
+
+    fig1 = plt.figure()
+    plt.hist2d(data['x'], data['y'], bins=[200, 200])     # 探测图像，可不看，仅供参考
+
+    frame = data['cmosfram'].values
+    x = data['x'].values
+    y = data['y'].values
+    fig2 = plt.figure()
+    frame_index = np.arange(np.min(frame), np.max(frame)+1, 1)
+    data_hist = np.histogram(frame, bins=frame_index)[0]
+    data_hist_all = data_hist
+    plt.plot(frame_index[:-1]-frame_index[0], data_hist, color='blue')
+#    plt.xlim(0,100000)
+    plt.ylim(0,20)
+    index = (np.abs(x-x0)<50) & (np.abs(y-y0)<50)
+    rawx0 = np.mean(data['rawx'].values[index])
+    rawy0 = np.mean(data['rawy'].values[index])
+    data_hist = np.histogram(frame[index], bins=frame_index)[0]
+    plt.plot(frame_index[:-1]-frame_index[0], data_hist, color='red')
+    plt.xlabel('Frame')
+    plt.ylabel('Counts')
+    # 全帧以及源的帧计数光变曲线，判断源的光变是否有单帧粒子数的增加引起
+
+    # 提取单帧图像，阈值这里设为20，具体参考上一步源光变曲线的计数
+    index_all = np.where(data_hist > cts_thres)[0]
+    if len(index_all)>0:
+        for i in index_all:
+            index = (frame == frame_index[i])
+
+            fig3 = plt.figure()
+            rawx = data['rawx'][index]
+            rawy = data['rawy'][index]
+            plt.hist2d(rawx, rawy, bins=[50, 50], range=[[rawx0-200, rawx0+200], [rawy0-200, rawy0+200]])  # 源图像
+            fig4 = plt.figure()
+            plt.hist2d(rawx, rawy, bins=[100, 100], range=[[0, 4096], [0, 4096]])  # 全帧图像
+
+    plt.show()
+
+
 def TA_quick(obsid,snum,root='',binsize=10,pha_file=None,rebin=2,grp=False,nH=None,group=None,rx=None,sep=True,ins='WXT',plotstyle='step',N=500,get_unabs=True,chatter=10,module='B'):
     """
     Perform quick analysis for TA.
